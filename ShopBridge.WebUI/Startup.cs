b1e883f26cb1243b5cad68d23011a7b974e99496
acer <acer@DESKTOP-S186E54>
2021-09-08 16:25:57 +0530

    Initial Solution Setup

---
+++ ShopBridge.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShopBridge.WebUI.Startup))]
+namespace ShopBridge.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
