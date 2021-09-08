794be85e957b2e5ac7838b8e5c46018250a9c550
acer <acer@DESKTOP-S186E54>
2021-09-09 02:18:45 +0530

    Added Entity Framework

---
+++ ShopBridge.DataAccess.SQL/Migrations/202109082032431_Initial.cs
@@ -0,0 +1,30 @@
+namespace ShopBridge.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        Name = c.String(maxLength: 20),
+                        Description = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Category = c.String(),
+                        Image = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+        }
+    }
+}
