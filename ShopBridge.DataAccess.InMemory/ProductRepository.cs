81b534e2ebd689ca4acb90529be7d6671d78d64e
acer <acer@DESKTOP-S186E54>
2021-09-08 18:26:43 +0530

    Product Model and InMemory Product Repository Built

---
+++ ShopBridge.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+
+namespace ShopBridge.DataAccess.InMemory
+{
+    public class ProductRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products = new List<Product>();
+
+    }
+}
